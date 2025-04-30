b601cb06b564c41f4dd1a48127c89e8b8b8ec433
reginabunch <78704272+reginabunch@users.noreply.github.com>
2025-04-30 15:25:01 -0400

    blog_alb

--- main.tf
+++ main.tf
@@ -64,19 +64,14 @@ module "blog_alb" {
     ex-http-https-redirect = {
       port     = 80
       protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
     }
   }
 
   target_groups = {
-    {
+    ex-instance = {
       name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
+      protocol         = "HTTP"
+      port             = 80
       target_type      = "instance"
     }
   }
