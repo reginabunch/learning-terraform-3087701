060a71a475212eb40ed525d0113b03a4d11f0bb4
reginabunch <78704272+reginabunch@users.noreply.github.com>
2025-04-30 15:22:25 -0400

    alb target groups

--- main.tf
+++ main.tf
@@ -43,15 +43,11 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-}
-
-resource "aws_autoscaling_attachment" "attachment" {
-  autoscaling_group_name  = module.autoscaling.name
-  alb_target_group_arn    = module.blog_alb.target_group_arns
 }
 
 module "blog_alb" {
@@ -77,10 +73,10 @@ module "blog_alb" {
   }
 
   target_groups = {
-    ex-instance = {
+    {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
     }
   }
