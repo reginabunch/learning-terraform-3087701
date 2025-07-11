06c711d366d0ac4857f32a3be7691cbe35d6f87e
Regina Bunch <REGINA_BUNCH@comcast.com>
2025-07-11 16:08:42 -0400

    lb

--- main.tf
+++ main.tf
@@ -59,26 +59,26 @@ module "blog_alb" {
 
 
 
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-       redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-  }
-
-  target_groups = {
-    ex-instance = {
+  target_groups = [
+    {
       name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
+      targets = {
+        target_id = aws_intance.blog.id
+        port = 80
+      }
     }
-  }
+  ]
+  
+  http_tcp_listeners = [
+    {
+      port          = 80
+      protocol      = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
     Environment = "dev"
